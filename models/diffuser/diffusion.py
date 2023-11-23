eda4c8f9055a179ae87d2873e8853f993277c95c
dobro12 <rlaehgudhoho@gmail.com>
2023-11-24 04:01:04 +0900

    modify diffusion.py

--- models/diffuser/diffusion.py
+++ models/diffuser/diffusion.py
@@ -128,8 +128,7 @@ class GaussianDiffusion(nn.Module):
             epsilon_uncond = self.model(x, cond, t, returns, force_dropout=True)
             epsilon = epsilon_uncond + self.condition_guidance_w*(epsilon_cond - epsilon_uncond)
         else:
-            # epsilon = self.model(x, cond, t)
-            epsilon = self.model(x, cond, t, t)
+            epsilon = self.model(x, cond, t)
 
         t = t.detach().to(torch.int64)
         x_recon = self.predict_start_from_noise(x, t=t, noise=epsilon)
@@ -143,7 +142,7 @@ class GaussianDiffusion(nn.Module):
                 x_start=x_recon, x_t=x, t=t)
         return model_mean, posterior_variance, posterior_log_variance
 
-    @torch.no_grad()
+    # @torch.no_grad()
     def p_sample(self, x, cond, t, returns=None):
         b, *_, device = *x.shape, x.device
         model_mean, _, model_log_variance = self.p_mean_variance(x=x, cond=cond, t=t, returns=returns)
@@ -152,7 +151,7 @@ class GaussianDiffusion(nn.Module):
         nonzero_mask = (1 - (t == 0).float()).reshape(b, *((1,) * (len(x.shape) - 1)))
         return model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
 
-    @torch.no_grad()
+    # @torch.no_grad()
     def p_sample_loop(self, shape, cond, returns=None, verbose=True, return_diffusion=False):
         device = self.betas.device
 
