20dd82d14f8fc880af8aac743a1586b418a3a113
dobro12 <rlaehgudhoho@gmail.com>
2023-11-24 04:51:12 +0900

    revert diffusion.py

--- models/diffuser/diffusion.py
+++ models/diffuser/diffusion.py
@@ -193,14 +193,6 @@ class GaussianDiffusion(nn.Module):
         nonzero_mask = (1 - (t == 0).float()).reshape(b, *((1,) * (len(x.shape) - 1)))
         return model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
 
-    def grad_p_sample2(self, x, cond, t, returns=None):
-        b, *_, device = *x.shape, x.device
-        model_mean, _, model_log_variance = self.p_mean_variance(x=x, cond=cond, t=t, returns=returns)
-        noise = 0.5*torch.randn_like(x)
-        # no noise when t == 0
-        nonzero_mask = (1 - (t == 0).float()).reshape(b, *((1,) * (len(x.shape) - 1)))
-        return model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
-
     def grad_p_sample_loop(self, shape, cond, returns=None, verbose=True, return_diffusion=False):
         device = self.betas.device
 
